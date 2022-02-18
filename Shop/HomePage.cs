aa60e7fbfbf7db657fc158a50674e8aa29a84078
lifeisbeautifful <42445537+lifeisbeautifful@users.noreply.github.com>
2022-02-18 15:33:42 +0200

    Update HomePage.cs

--- Shop/HomePage.cs
+++ Shop/HomePage.cs
@@ -14,11 +14,11 @@ namespace Shop
             Driver = driver;
         }
 
-        IWebElement signIn => Driver.FindElement(By.LinkText("Sign in"));
+        IWebElement signAn => Driver.FindElement(By.LinkText("Sign in"));
         IWebElement emailField => Driver.FindElement(By.Id("email_create"));
         IWebElement createBtn => Driver.FindElement(By.Id("SubmitCreate"));
         IWebElement signInForm => Driver.FindElement(By.Id("account-creation_form"));
-        public void clickSignIn() => signIn.Click();
+        public void clickSignIn() => signAn.Click();
         public bool CreateAccPage() { return emailField.Displayed; }
         public bool CreateAccount(string email)
         {
